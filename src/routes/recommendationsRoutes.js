504a5773c18ad9dddc412e8b821ef9b99a82c95e
Oshadhi Kaawshani <165757418+oshadhikawshani@users.noreply.github.com>
2025-07-27 01:29:02 +0530

    Initial commit

--- src/routes/recommendationsRoutes.js
+++ src/routes/recommendationsRoutes.js
@@ -5,6 +5,8 @@ const authMiddleware = require('../middlewares/authMiddleware');
 
 // Get recommendations for the user
 router.get('/', authMiddleware, recommendationsController.getRecommendations);
+// Personalized recommendations endpoint (alias for the main endpoint)
+router.get('/personalized', authMiddleware, recommendationsController.getRecommendations);
 router.get('/careers', recommendationsController.getAllCareers);
 // Get all careers with match scores for authenticated users
 router.get('/careers-with-scores', authMiddleware, recommendationsController.getAllCareersWithScores);
